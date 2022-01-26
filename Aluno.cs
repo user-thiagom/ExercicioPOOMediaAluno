9056df1b651fa1ea1cc28457a0088da5897d5838
Thiago Mateus <thiagomateus123@hotmail.com>
2022-01-26 16:28:39 -0300

    Finalização da atividade

---
+++ Aluno.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercicioPOOMediaAluno
+{
+    public class Aluno
+    {
+        public string Nome;
+        public double Nota1;
+        public double Nota2;
+        public double Nota3;
+        public bool Aprovado;
+
+        public Aluno()
+        {
+            Aprovado = false;
+        }
+
+        public double CalcularNotaFinal()
+        {
+            double notafinal = Nota1 + Nota2 + Nota3;
+            if (notafinal >= 60)
+                Aprovado = true;
+            return notafinal;
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,26 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+using ExercicioPOOMediaAluno;
+
+Aluno aluno = new Aluno();
+
+Console.WriteLine("Digite o nome do Aluno: ");
+aluno.Nome = Console.ReadLine();
+
+Console.WriteLine("Digite as três notas do aluno: ");
+aluno.Nota1 = double.Parse(Console.ReadLine());
+aluno.Nota2 = double.Parse(Console.ReadLine());
+aluno.Nota3 = double.Parse(Console.ReadLine());
+
+Console.WriteLine($"NOTA FINAL: {aluno.CalcularNotaFinal()}");
+
+switch (aluno.Aprovado)
+{
+    case true:
+        Console.WriteLine("APROVADO");
+        break;
+    case false:
+        Console.WriteLine("REPROVADO");
+        Console.WriteLine($"FALTARAM {(60 - aluno.CalcularNotaFinal())} PONTOS");
+        break;
+}
